dc325e37e2b619560743a64d4d1bd6d0c3c5ee18
shafanaurra <shafa.nauraln@gmail.com>
2024-12-09 11:03:07 +0700

    add relations

--- database/migrations/2023_01_05_052906_create_table_parkir.php
+++ database/migrations/2023_01_05_052906_create_table_parkir.php
@@ -15,13 +15,16 @@ class CreateTableParkir extends Migration
     {
         Schema::create('parkir', function (Blueprint $table) {
             $table->id();
-            $table->integer('id_user')->nullable();
-            $table->integer('id_tarif');
+            $table->unsignedBigInteger('id_user');
+            $table->unsignedBigInteger('id_tarif');
             $table->string('kode_parkir')->unique();
             $table->string('durasi', 50);
             $table->integer('harga');
             $table->tinyInteger('status');
             $table->timestamps();
+
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('id_tarif')->references('id')->on('tarif')->onDelete('cascade');
         });
     }
 
@@ -32,6 +35,10 @@ class CreateTableParkir extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('parkir');
+        Schema::table('parkir', function (Blueprint $table) {
+            $table->dropForeign(['id_user']); // Drop foreign key id_user
+            $table->dropForeign(['id_tarif']); // Drop foreign key id_tarif
+        });
+        Schema::dropIfExists('parkir'); // Hapus tabel parkir
     }
 }
